818ed79e098b0fc14dc3758e7dc4bb46642e1f71
prettydude <coolboy.MC1@gmail.com>
2021-05-01 01:21:42 +0300

    add github origin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://rubygramreact.heroku.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://rubygramreact.herokuapp.com/', 'http://localhost:3000' ]
+  config.action_cable.allowed_request_origins = [ 'https://rubygramreact.herokuapp.com/', 'http://localhost:3000', "https://prettydude.github.io/" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
